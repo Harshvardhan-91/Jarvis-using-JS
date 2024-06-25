5d25a2ad82f6328e6ca74f3d7a594c4bde8baa79
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-25 23:17:22 +0530

    added

--- script.js
+++ script.js
@@ -14,6 +14,29 @@ recognition.onstart = function () {
   console.log("vr active");
 };
 
+//sr result
+recognition.onresult = function (event) {
+  let current = event.resultIndex;
+  let transcript = event.results[current][0].transcript;
+  transcript = transcript.toLowerCase();
+  console.log(`my words: ${transcript}`);
+  if (transcript.includes("hello jarvis")) {
+    readOut("hello sir");
+  }
+  if (transcript.includes("open youtube")) {
+    readOut("opening youtube");
+    window.open("https://www.youtube.com");
+  }
+  if (transcript.includes("open google")) {
+    readOut("opening google");
+    window.open("https://www.google.com");
+  }
+  if (transcript.includes("open firebase")) {
+    readOut("opening firebase console");
+    window.open("https://console.firebase.google.com/");
+  }
+};
+
 //speech recoginition stop
 recognition.onend = function () {
   console.log("vr deactive");
@@ -35,7 +58,7 @@ function readOut(message) {
   //different voices
   const allVoices = speechSynthesis.getVoices();
   speech.text = message;
-  speech.voice = allVoices[7];
+  speech.voice = allVoices[0];
   speech.volume = 1;
   window.speechSynthesis.speak(speech);
   console.log("speaking out");
